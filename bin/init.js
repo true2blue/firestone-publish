3a8af842da5d3491e0076364ed8010ce659b6d21
benjaminl <benjamin.li@ehealth.com>
2019-11-21 13:34:50 +0800

    refactor

--- bin/init.js
+++ bin/init.js
@@ -8,10 +8,10 @@ db.configmocks.insert({
     "maxBuyNum" : 3,
     "curBuyNum" : 0,
     "ths_url" : "http://mncg.10jqka.com.cn/cgiwt/index/index",
-    "username" : "xxxxx",
-    "cookie" : "xxxxx",
-    "gdzh" : "xxxxx",
-    "sh_gdzh" : "xxxxx",
+    "cookie" : "__utma=156575163.1101180334.1557107567.1557375466.1557738304.3; __utmz=156575163.1557738304.3.3.utmcsr=yamixed.com|utmccn=(referral)|utmcmd=referral|utmcct=/fav/article/2/157; isSaveAccount=0; PHPSESSID=0ee41a659eca05d9c02da980b42b337b; Hm_lvt_78c58f01938e4d85eaf619eae71b4ed1=1570773015,1570773052,1572246079; user=MDphcXVhSVFjOjpOb25lOjUwMDo0MjUzOTk0Njc6NywxMTExMTExMTExMSw0MDs0NCwxMSw0MDs2LDEsNDA7NSwxLDQwOzEsMSw0MDsyLDEsNDA7MywxLDQwOzUsMSw0MDs4LDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAxLDQwOjI0Ojo6NDE1Mzk5NDY3OjE1NzIyNDYwOTc6OjoxNTA2MDQ4OTYwOjg2NDAwOjA6MThkZjg4MDU4NWQ1MjAxNWI4YjczMmQ1YjVjY2Q4MjdiOmRlZmF1bHRfMjox; userid=415399467; u_name=aquaIQc; escapename=aquaIQc; ticket=4946f5e543fb81ad0500537a32fbb290; log=; v=ApOFSy-Cygxe94ZYp7X7ueKFIhy-SCcK4dxrPkWw77LpxL1KzRi3WvGs-4FW; Hm_lpvt_78c58f01938e4d85eaf619eae71b4ed1=1572246108",
+    "username" : "48039195",
+    "gdzh" : "0098894246",
+    "sh_gdzh" : "A474614369",
     "userId" : ObjectId("5d905db9fc84d3224b0eb59c")
 });
 db.strategies.insertMany([{
@@ -39,7 +39,7 @@ db.strategies.insertMany([{
 },{
     "_id" : ObjectId("5da19b7d181fc3600c5544c3"),
     "name" : "异动拉升",
-    "description" : "在<i>监控时间</i>范围内,当<i>大盘涨幅</i>处于指定范围，并且当前<i>个股涨幅</i>处于指定范围，在<i>涨速时间</i>内，<i>涨幅拉升达</i>到预期值则买入股票",
+    "description" : "在<i>监控时间</i>范围内,当<i>大盘涨幅</i>处于指定范围，并且当前<i>个股涨幅</i>处于指定范围，在<i>涨速时间</i>内，<i>涨幅</i>，<i>成交额</i>达到预期值则买入股票",
     "op" : "buy",
     "url" : "Ydls",
     "parameters" : {
@@ -58,7 +58,8 @@ db.strategies.insertMany([{
         },
         "speed" : {
             "time" : "2",
-            "percent" : "2"
+            "percent" : "2",
+            "amount" : "300"
         },
         "volume" : 100
     }
--- build.bat
+++ build.bat
@@ -1,11 +1,18 @@
-rem REM build firestone
+REM load init data
+RMDIR bin /S /Q && MKDIR bin
+xcopy ..\firestone-server\install\init.js bin /i /e /y
+xcopy ..\firestone-server\install\init.bat bin /i /e /y
+cd bin && start /W init.bat && cd..
+
+REM build firestone
 start /W build-firestone.bat
 
-rem REM build firestone-server
+REM build firestone-server
 start /W build-firestone-server.bat
 
 REM build firestone-engine
 start /W build-firestone-engine.bat
 
+REM zip file
 del firestone-v1.0.zip /Q
 7z a -tzip firestone-v1.0.zip bin client server README.md startup.bat
