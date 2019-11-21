0db89c9521677e7327153d3c1c4f19ed33ef2f53
benjaminl <benjamin.li@ehealth.com>
2019-11-21 13:42:59 +0800

    disable the ask target is file or directory

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 REM load init data
 RMDIR bin /S /Q && MKDIR bin
-xcopy ..\firestone-server\install\init.js bin /i /e /y
-xcopy ..\firestone-server\install\init.bat bin /i /e /y
+xcopy ..\firestone-server\install\init.js bin\ /i /e /y
+xcopy ..\firestone-server\install\init.bat bin\ /i /e /y
 cd bin && start /W init.bat && cd..
 
 REM build firestone
