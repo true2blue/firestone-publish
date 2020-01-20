8a32c6854b61955c8f5e1dacdce49769bf112fe0
benjaminl <benjamin.li@ehealth.com>
2020-01-20 15:36:44 +0800

    do not automatically reload the codes and concept

--- bin/init.bat
+++ bin/init.bat
@@ -1,3 +1,3 @@
-mongo 127.0.0.1/firestone --eval "db.dropDatabase(); db.getSiblingDB('firestone');" "init.js" 
-mongoimport -d firestone -c codes "codes.json"
-mongoimport -d firestone -c concepts "concepts.json" && exit
+mongo 127.0.0.1/firestone --eval "db.dropDatabase(); db.getSiblingDB('firestone');" "init.js" && exit
+REM mongoimport -d firestone -c codes "codes.json"
+REM mongoimport -d firestone -c concepts "concepts.json"
