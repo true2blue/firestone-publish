f513f956218c0dd528bb7143820d0d48ec664183
true2blue <842958037@qq.com>
2021-04-07 20:29:35 +0800

    refact publish

--- bin/init.bat
+++ bin/init.bat
@@ -1,3 +1,1 @@
-mongo 127.0.0.1/firestone --eval "db.dropDatabase(); db.getSiblingDB('firestone');" "init.js" && exit
-REM mongoimport -d firestone -c codes "codes.json"
-REM mongoimport -d firestone -c concepts "concepts.json"
+mongo 127.0.0.1/firestone --eval "db.dropDatabase(); db.getSiblingDB('firestone');" "c:/aqua/firestone-server/install/initData.js" && exit
