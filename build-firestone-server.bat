4912459ac708751aba44e2fba813bfd1a92ba2c7
true2blue <842958037@qq.com>
2021-04-07 20:56:48 +0800

    revert

--- build-firestone-server.bat
+++ build-firestone-server.bat
@@ -1,3 +1,3 @@
 RMDIR server /S /Q
 cd ..\firestone-server
-npm run build && xcopy dist ..\firestone-publish\server /i /e /y && xcopy node_modules ..\firestone-publish\server\node_modules /i /e /y && copy .env ..\firestone-publish\server /y && exit
+npm run build && xcopy dist ..\firestone-publish\server /i /e /y && exit
