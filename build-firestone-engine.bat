d1e24b485e5769cccb61db4392b3060faf231209
benjaminl <benjamin.li@ehealth.com>
2021-04-08 14:29:51 +0800

    create logs folder

--- build-firestone-engine.bat
+++ build-firestone-engine.bat
@@ -6,4 +6,5 @@ xcopy dist\main.exe ..\firestone-server\shell /i /e /y
 xcopy dist\calculate.exe ..\firestone-server\shell /i /e /y 
 cd ..\firestone-server
 xcopy shell ..\firestone-publish\server\shell /i /e /y
+mkdir ..\firestone-publish\server\shell\logs
 del ..\firestone-publish\server\shell\logs\*.* /Q && exit
