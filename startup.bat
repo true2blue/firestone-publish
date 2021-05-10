4d2354caf495d656b5a571929c999ea92b04049e
true2blue <842958037@qq.com>
2021-05-10 21:49:11 +0800

    add real

--- startup.bat
+++ startup.bat
@@ -3,4 +3,5 @@ start mongod --storageEngine inMemory --port 27018 --dbpath C:/data/dbram
 timeout 5
 start mongo 127.0.0.1:27018/firestone-data --eval "db.dropDatabase(); db.getSiblingDB('firestone-data');" "./bin/initFirestoneData.js"
 start serve -s client
+start C:/同花顺软件/同花顺/xiadan.exe
 cd server && node index.js
