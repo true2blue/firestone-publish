737045b499e014a2b234fabb64c9068b6af7c325
true2blue <842958037@qq.com>
2021-05-11 20:13:27 +0800

    copy trading.py before install

--- build-firestone-engine.bat
+++ build-firestone-engine.bat
@@ -3,7 +3,8 @@ git pull
 pipenv run pyinstaller -F --clean main.spec
 pipenv run pyinstaller -F --clean calculate.spec
 xcopy dist\main.exe ..\firestone-server\shell /i /e /y
-xcopy dist\calculate.exe ..\firestone-server\shell /i /e /y 
+xcopy dist\calculate.exe ..\firestone-server\shell /i /e /y
+xcopy lib\trading.py C:\Users\Administrator\.virtualenvs\firestone-engine-bHojJoF3\Lib\site-packages\tushare\stock /i /e /y
 cd ..\firestone-server
 xcopy shell ..\firestone-publish\server\shell /i /e /y
 mkdir ..\firestone-publish\server\shell\logs
