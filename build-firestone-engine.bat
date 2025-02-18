dc5c3f3c1c681fa9e999b4b41d4f175a91ea153d
true2blue <842958037@qq.com>
2025-02-18 18:32:54 +0800

    cookie

--- build-firestone-engine.bat
+++ build-firestone-engine.bat
@@ -5,6 +5,7 @@ pipenv run pyinstaller -F --clean main.spec
 pipenv run pyinstaller -F --clean calculate.spec
 xcopy dist\main.exe ..\firestone-server\shell /i /e /y
 xcopy dist\calculate.exe ..\firestone-server\shell /i /e /y
+xcopy dist\CookieManager.exe ..\firestone-server\shell /i /e /y
 cd ..\firestone-server
 xcopy shell ..\firestone-publish\server\shell /i /e /y
 mkdir ..\firestone-publish\server\shell\logs
