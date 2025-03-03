ee3d648bfc0173c55a9402fe333bfe7291ca5a40
true2blue <842958037@qq.com>
2025-03-03 20:02:54 +0800

    init

--- startup.bat
+++ startup.bat
@@ -5,5 +5,4 @@ start mongod --storageEngine inMemory --port 27018 --dbpath C:/data/dbram
 timeout 5
 start mongo 127.0.0.1:27018/firestone-data --eval "db.dropDatabase(); db.getSiblingDB('firestone-data');" "./bin/initFirestoneData.js"
 start serve -s client
-start C:/ͬ��˳����/ͬ��˳/xiadan.exe
 cd server && node index.js
