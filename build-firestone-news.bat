cc4d195314877fe29063b40bd6b5352bd0eb1f0f
true2blue <842958037@qq.com>
2025-10-11 22:00:20 +0800

    init

--- build-firestone-news.bat
+++ build-firestone-news.bat
@@ -1,2 +1,2 @@
 cd ..\firestone-news
-cd venv\Scripts && .\activate.bat && cd ..\.. && pip install pyinstaller && pyinstaller --onefile src/app.py && xcopy dist\app.exe ..\firestone-publish\news /i /e /y && del ..\firestone-publish\news\logs\*.* /Q && exit
+cd venv\Scripts && .\activate.bat && cd ..\.. && pip install pyinstaller && pyinstaller --onefile src/app.py && xcopy dist\app.exe ..\firestone-publish\news /i /e /y && pyinstaller --onefile src/getRealtimeNews.py && xcopy dist\getRealtimeNews.exe ..\firestone-publish\news /i /e /y && del ..\firestone-publish\news\logs\*.* /Q && exit
