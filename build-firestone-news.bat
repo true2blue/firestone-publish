ff112cc731949e35321ff393c8ea825e5868c632
true2blue <842958037@qq.com>
2025-10-11 22:25:33 +0800

    add config

--- build-firestone-news.bat
+++ build-firestone-news.bat
@@ -1,2 +1,3 @@
 cd ..\firestone-news
+xcopy config\config.ini ..\firestone-publish\news\config /i /e /y
 cd venv\Scripts && .\activate.bat && cd ..\.. && pip install pyinstaller && pyinstaller --onefile src/app.py && xcopy dist\app.exe ..\firestone-publish\news /i /e /y && pyinstaller --onefile src/getRealtimeNews.py && xcopy dist\getRealtimeNews.exe ..\firestone-publish\news /i /e /y && del ..\firestone-publish\news\logs\*.* /Q && exit
